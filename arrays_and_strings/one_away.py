bedc09995e3afee2e5160ae393dc205ce1745770
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-05-23 11:26:42 -0600

    Solved one_away challenge using sets an iteration

--- arrays_and_strings/one_away.py
+++ arrays_and_strings/one_away.py
@@ -33,34 +33,107 @@ def one_away(str1, str2):
     >>> one_away("pal", "palks")
     False
     """
-    
+
+    str1_chars = set(str1)
+    str2_chars = set(str2)
+
     different = 0
 
+    # if the stings are the same, return true
     if str1 == str2:
         return True
 
-    if abs(len(str1) - len(str2)) > 1:
-        return False
-
-    if (len(str1) - 1 == len(str2)) or (len(str2) - 1 == len(str1)):
-        if len(str1) < len(str2):
-            for char in str1:
-                if char not in str2:
-                    return False
-
-        else:
-            for char in str2:
-                if char not in str1:
-                    return False
-
-    elif len(str1) == len(str2):
+    # if lengths of the strings are equal, they must have all but one same charecters
+    if len(str1) == len(str2):
         for char in str1:
-            if char not in str2:
+            if char not in str2_chars:
                 different += 1
 
-                if different > 1:
-                    return False
-    return True
+        if different > 1:
+            return False
+
+        else:
+            return True
+    # if length of string 1 is one greater than string 2, all chars in string 2 must be in string 1
+    elif len(str1) == (len(str2) + 1):
+        for char in str2:
+            if char not in str1_chars:
+                return False
+            else:
+                return True
+
+    # if the length of string 2 is greater than string 1 by 1 charecter
+    elif len(str2) == (len(str1) + 1):
+        for char in str1:
+            if char not in str2_chars:
+                return False
+            else:
+                return True
+
+    else:
+        return False
+
+
+# print one_away("pale", "bale")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+#     # different = 0
+
+#     # if str1 == str2:
+#     #     return True
+
+#     # if abs(len(str1) - len(str2)) > 1:
+#     #     return False
+
+#     # if (len(str1) - 1 == len(str2)) or (len(str2) - 1 == len(str1)):
+#     #     if len(str1) < len(str2):
+#     #         for char in str1:
+#     #             if char not in str2:
+#     #                 return False
+
+#     #     else:
+#     #         for char in str2:
+#     #             if char not in str1:
+#     #                 return False
+
+#     # elif len(str1) == len(str2):
+#     #     for char in str1:
+#     #         if char not in str2:
+#     #             different += 1
+
+#     #             if different > 1:
+#     #                 return False
+#     # return True
 
 
 if __name__ == "__main__":
