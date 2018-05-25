32243bc46a6aee2d845d64ceed083c743e564c9b
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-05-25 09:48:00 -0700

    Redid rotate matrix algorithm

--- arrays_and_strings/rotate_matrix.py
+++ arrays_and_strings/rotate_matrix.py
@@ -1,35 +1,63 @@
+# def rotate_matrix(matrix):
+#     """Rotates a matrix 90 degrees clockwise"""
+
+#     length_matrix = len(matrix)
+
+#     for layer in range(length_matrix//2):
+
+#         first = layer
+#         last = length_matrix - layer - 1
+
+#         for i in range(first, last):
+#             # save the top value
+#             top = matrix[layer][i]
+
+#             # move bottom left to top
+#             matrix[layer][i] = matrix[-i - 1][layer]
+
+#             # move the botom right to the bottom left
+#             matrix[-i - 1][layer] = matrix[-layer -1][-i - 1]
+
+#             # move the top right to bottom left
+#             matrix[-layer -1][-i - 1] = matrix[i][- layer - 1]
+
+#             # replace the top to the right 
+#             matrix[i][- layer - 1] = top
+
+#     return matrix
+
+# print rotate_matrix([["a","b","c"],
+#           [1,2,3],
+#           ["x","y","z"]])
+
+
 def rotate_matrix(matrix):
     """Rotates a matrix 90 degrees clockwise"""
 
-    length_matrix = len(matrix)
+    length = len(matrix)
 
-    for layer in range(length_matrix//2):
-
+    for layer in range(length//2):
         first = layer
-        last = length_matrix - layer - 1
+        last = length - layer - 1
 
         for i in range(first, last):
-            # save the top value
-            top = matrix[layer][i]
+            # save the top left
+            top_left = matrix[layer][i]
 
-            # move bottom left to top
+            # switch bottom left to top left
             matrix[layer][i] = matrix[-i - 1][layer]
 
-            # move the botom right to the bottom left
-            matrix[-i - 1][layer] = matrix[-layer -1][-i - 1]
+            #move bottom right to bottom left
+            matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
 
             # move the top right to bottom left
-            matrix[-layer -1][-i - 1] = matrix[i][- layer - 1]
+            matrix[-layer - 1][-i -1] = matrix[i][-layer - 1]
 
-            # replace the top to the right 
-            matrix[i][- layer - 1] = top
+            # replace the top right
+            matrix[i][-layer - 1] = top_left
 
     return matrix
 
 print rotate_matrix([["a","b","c"],
           [1,2,3],
           ["x","y","z"]])
-
-
-
-# https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter1/7_Rotate%20Matrix/RotateMatrix.py
