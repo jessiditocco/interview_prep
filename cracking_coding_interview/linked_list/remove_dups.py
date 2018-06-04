33aa822cdfec9be43534c2d3cc15870cc82bd40b
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-06-04 12:22:16 -0700

    Wrote algorithm that removes duplicates and updates the tail with new tail in ll

--- cracking_coding_interview/linked_list/remove_dups.py
+++ cracking_coding_interview/linked_list/remove_dups.py
@@ -10,7 +10,7 @@ class Node(object):
 
 
 class LinkedList(object):
-    def __init__(self, head, tail):
+    def __init__(self, head, tail=None):
         self.head = head
         self.tail = tail
 
@@ -21,16 +21,46 @@ class LinkedList(object):
 
 
 
-def remove_dups(self):
-    dup_counts = {}
+    def remove_dups(self):
+        # import pdb; pdb.set_trace()
 
-    current = self.head
+        if ll.head is None:
+            return
 
-    while current.next is not None:
-        dup_counts[current.data] = dup_counts.get(current.data, 0) + 1 
+        dups = set()
 
-        if current.next.data in dup_counts:
-            current.next = current.next.next
+        current = self.head
 
-        else:
-            current = current.next
+        while current.next is not None:
+            dups.add(current.data)
+
+            if current.next.data in dups:
+                current.next = current.next.next
+                
+                if current.next is None:
+                    self.tail = current
+
+            else:
+                current = current.next
+
+
+if __name__ == "__main__":
+    n1 = Node("apple")
+    n2 = Node("berry")
+    n3 = Node("cherry")
+    n4 = Node("berry")
+    n5 = Node("apple")
+    n6 = Node("apple")
+    n7 = Node("berry")
+    n8 = Node("durian")
+    n1.next = n2
+    n2.next = n3
+    n3.next = n4
+    n4.next = n5
+    n5.next = n6
+    n6.next = n7
+    n7.next = n8
+
+    ll = LinkedList(n1, n8)
+
+    # ll.remove_dups()
