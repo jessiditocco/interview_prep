4e421db2052d5a44bc86cf612587efbcb5dc1aa1
Jessi DiTocco <jessi.ditocco@gmail.com>
2018-06-06 17:28:06 -0700

    Solved day 7 of code: arrays

---
+++ hackerrank/30_days_code/arrays.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+n = int(raw_input())
+arr = map(int, raw_input().rstrip().split())
+
+for i in range(n):
+    print arr[n - i - 1],
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    arr = map(int, raw_input().rstrip().split())
